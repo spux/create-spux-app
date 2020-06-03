86c2aaba52e6a525a8aa09530db539b665e828f6
Melvin <melvincarvalho@gmail.com>
2020-06-03 19:53:20 +0200

    fix pipe bug

--- bin/create-spux-app.js
+++ bin/create-spux-app.js
@@ -4,12 +4,12 @@ var shell = require('shelljs')
 
 globalThis.data = {
   template: 'https://spux.org/index.html',
-  to: 'hello.html'
+  to: './hello.html'
 }
 
 data.to = process.argv[2] || data.to
 data.template = process.argv[3] || data.template
 
-shell.exec('curl ' + data.template).to(process.argv[2] || data.template)
+shell.exec('curl ' + data.template).to(data.to)
 
 shell.echo(`created spux app with template ${data.template} at ${data.to}`)
